dca03e81378a9c967cd239646dfc4561aa0728bd
Donggeon Byeon <dongb94@gmail.com>
2019-09-24 22:35:07 +0900

    Add Node and Update Grid

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,5 +1,23 @@
 
-public class Grid
+using UnityEngine;
+
+public class Grid : MonoBehaviour
 {
+    public LayerMask unwalkalbeMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    private Node[,] grid;
+
+    private float nodeDiameter;
+    private int gridSizeX, gridSizeY;
     
+    private void Start()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+    }
 }
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+public class Node
+{
+    private bool isWalkable;
+    private Vector3 worldPosition;
+    
+    
+}
